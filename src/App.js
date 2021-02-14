96c4801cf1223bd99b551eb9f1eabea0b1b03ece
Sunil <sunilsv26@gmail.com>
2021-02-14 11:56:51 +0530

    #8 Rendered auth component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
+import Layout from './layout/layout'
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-     
+     <Layout />
     </div>
     </BrowserRouter>
   );
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route} from 'react-router-dom';
+import Auth from '../auth/auth'
 
 const layout=(props)=>{
     return(
         <Switch>
+            <Route path='/auth' component={Auth} />
         </Switch>
     )
 }
