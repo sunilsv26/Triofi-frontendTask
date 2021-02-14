b4dfefdce078b1eea42056f0f170b99417d63d72
Sunil <sunilsv26@gmail.com>
2021-02-14 11:47:24 +0530

    #4 imported browserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
      
     </div>
+    </BrowserRouter>
   );
 }
 
