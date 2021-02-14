9cd7688f6b668f87828b39f76bca442bc7649010
Sunil <sunilsv26@gmail.com>
2021-02-14 12:07:53 +0530

    #12 rendered admin component

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import Auth from '../auth/auth';
-import User from '../user/user'
+import User from '../user/user';
+import Admin from '../admin/admin'
 
 const layout=(props)=>{
     return(
         <Switch>
             <Route path='/user' component={User} />
+            <Route path='/admin' component={Admin} />
             <Route path='/' component={Auth} />
         </Switch>
     )
